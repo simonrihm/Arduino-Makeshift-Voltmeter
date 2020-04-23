aa47de947676560fdd5e94321f098165dee83ca7
Simon <Simon@DESKTOP-PKQK03O>
2020-04-23 10:54:24 +0200

    Added README and commented Arduino code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Arduino-Makeshift-Voltmeter
+# Arduino Makeshift Voltmeter
+The analog input pins of an Arduino are used to do crude voltage measurements within 5V systems.
+The values are then conveniently displayed to a 16x2 LCD screen and updated live.
+
+## Details
+Arduino systems have some analog pins, the Arduino Uno has 6 for example. If no voltage meter is available and only rough estimates are needed, these can be used to measure voltages from 0 to 5 V - most of the times this will be within the same Arduino sytem. The resolution is 1024, so a value of 1023 equals 5 V. 
+The input pin value is read and converted to voltage by multiplying 5V/1023. Both raw and converted data can be seen on serial port at 9600 Baud when connected to PC.
+The LCD screen is used to display 4 voltages live. Background light contrast can be adjusted with potentiometer. This is a fast way for rudimentary voltage measurement in simple DC circuits with 5V or lower, esp. powered by Arduino. It helped me a lot when I was learning about electronics, was building some test circuits and didn't have a proper voltage meter yet.
+
+__Attention!__
+* If you use a voltage source other than Arduino itself, make sure that it does not exceed 5V!
+* The resolution is very coarse and big errors are expected. Do not use when exact values are needed!
+
+## Components
+* Arduino Uno (I used Elegoo Uno R3)
+* 10k potentiometer (I used kwmobile B10K)
+* 16x2 LCD screen (I used Qapass 1602A)
+
+## Circuit schema
+![alt text](voltmeter_circuit/VoltageMeter_Arduino.png "Breadboard circuit schema")
+
--- VoltMeter_Arduino/voltmeter_arduino.ino
+++ VoltMeter_Arduino/voltmeter_arduino.ino
@@ -1,6 +1,8 @@
+// Initialize LCD screen
 #include <LiquidCrystal.h>
 LiquidCrystal LCD(7, 8, 9, 10, 11, 12);
 
+// Set analog input pins
 int sensorPin[4] = {
   A0,
   A1,
@@ -8,6 +10,7 @@ int sensorPin[4] = {
   A3
 };
 
+// Raw data read by pins
 int sensorRaw[4] = {
   0,
   0,
@@ -15,6 +18,7 @@ int sensorRaw[4] = {
   0
 };
 
+// Converted voltage read from pins
 float sensorValue[4] = {
   0,
   0,
@@ -22,6 +26,7 @@ float sensorValue[4] = {
   0
 };
 
+// Output string for LCD screen
 char output[8] = {
   '0',
   ')',
@@ -32,24 +37,37 @@ char output[8] = {
   'X',
   ' '
 };
+
+// Voltage values as output string for Serial Monitor
 char valstr[4];
 
+// Update interval in seconds
+int updateTime = 1;
+
 void setup() {
+  // Initialize Serial Monitor at 9600 Baud
   Serial.begin(9600);
-  
+
+  // Initialize LCD screen and display start message
   LCD.begin(16,2);
   LCD.print("INIT MEASUREMENT");
   LCD.setCursor(0,1);
-  LCD.print("UPDATE EVERY 1 S");
+  LCD.print("UPDATE EVERY X S");
+  LCD.setCursor(13,1);
+  LCD.print(updateTime);
 
+  // Wait 2 seconds before measurements start
   delay(2000);
 }
 
 void loop() {
-  
+  // Read input pins one after another
   for (int i = 0; i < 4; i++){
+    // Read raw data and convert to Volt
     sensorRaw[i] = analogRead(sensorPin[i]);
     sensorValue[i] = sensorRaw[i] / 1023.0 * 5;
+
+    // Print raw and converted data to Serial Monitor
     Serial.print(i+1);
     Serial.print(": ");
     Serial.print(sensorRaw[i]);
@@ -57,6 +75,7 @@ void loop() {
     Serial.print(sensorValue[i]);
     Serial.print(", ");
     
+    // Set position on LCD screen and display voltage
     LCD.setCursor((i%2)*8,(i/2));
     dtostrf(sensorValue[i],4,2,valstr);
     output[0] = (i+1) + '0';
@@ -66,6 +85,7 @@ void loop() {
     LCD.print(output);
   }
 
+  // End line on serial Monitor and wait for next measurement
   Serial.println("");
-  delay(1000);
+  delay(updateTime*1000);
 }
